6581a61d29048037647a3d26ff737d2874ace045
JuliaSivridi <julia.sivridi@gmail.com>
2026-05-06 01:16:23 +0300

    Fix router order for game buttons

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -6,6 +6,6 @@ from app.handlers.tictactoe import router as tictactoe_router
 
 
 def register_routers(dispatcher: Dispatcher) -> None:
-    dispatcher.include_router(common_router)
     dispatcher.include_router(tictactoe_router)
     dispatcher.include_router(minesweeper_router)
+    dispatcher.include_router(common_router)
